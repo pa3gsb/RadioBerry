88935adb6c2e50da9302200a70a3b4607ecede3f
PA3GSB <admin@pa3gsb.nl>
2017-02-25 10:57:29 +0100

    2-receivers

--- Firmware/radioberry.v
+++ Firmware/radioberry.v
@@ -25,7 +25,7 @@ clk_10mhz,
 ad9866_clk, ad9866_adio,ad9866_rxen,ad9866_rxclk,ad9866_txen,ad9866_txclk,ad9866_sclk,ad9866_sdio,ad9866_sdo,ad9866_sen_n,ad9866_rst_n,ad9866_mode,ad9866_pga,	
 spi_sck, spi_mosi, spi_miso, spi_ce,   
 DEBUG_LED1,DEBUG_LED2,DEBUG_LED3,DEBUG_LED4,
-rxFIFOEmpty,
+rx1_FIFOEmpty, rx2_FIFOEmpty,
 txFIFOFull,
 ptt_in,
 ptt_out,
@@ -51,7 +51,8 @@ input wire spi_sck;
 input wire spi_mosi; 
 output wire spi_miso; 
 input [1:0] spi_ce; 
-output wire rxFIFOEmpty;
+output wire rx1_FIFOEmpty;
+output wire rx2_FIFOEmpty;
 output wire txFIFOFull;
 
 output  wire  DEBUG_LED1;  
@@ -74,7 +75,7 @@ reg 	randomize;				// if randomize is checked (eg in powersdr) the agc value is 
 									// if randomize is not checked (eg in powersdr) the gain value (inversie van s-att) is used for gain
 
 //Debug	
-assign DEBUG_LED3 =  (rxfreq == 32'd3630000) ? 1'b1:1'b0; 
+assign DEBUG_LED3 =  (rx1_freq == 32'd3630000) ? 1'b1:1'b0; 
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 //                         Receive Testing....compile time setup.
@@ -164,11 +165,14 @@ wire spi_done;
 always @ (posedge spi_done)
 begin	
 	if (!ptt_in) begin
-		rxfreq <= spi_recv[31:0];
+		rx1_freq <= spi_recv[31:0];
+		rx1_speed <= spi_recv[41:40];
+		
+		//following settings are from the active selected receiver! transferred with the rx1 data
 		att <= spi_recv[36:32];
 		dither <= spi_recv[37];
 		randomize <= spi_recv[38];
-		speed <= spi_recv[41:40];
+		
 	end else begin
 		tx_gain <= spi_recv[37:32];
 	end
@@ -176,31 +180,69 @@ end
 
 spi_slave spi_slave_rx_inst(.rstb(!reset),.ten(1'b1),.tdata(rxDataFromFIFO),.mlb(1'b1),.ss(spi_ce[0]),.sck(spi_sck),.sdin(spi_mosi), .sdout(spi_miso),.done(spi_done),.rdata(spi_recv));
 
+
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
-//                         Decimation Rate Control
+//                         SPI Control rx2
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+wire [47:0] spi_rx2_recv;
+wire spi_rx2_done;
+
+always @ (posedge spi_rx2_done)
+begin	
+		rx2_freq <= spi_rx2_recv[31:0];
+		rx2_speed <= spi_rx2_recv[41:40];
+end 
+
+spi_slave spi_slave_rx2_inst(.rstb(!reset),.ten(1'b1),.tdata(rx2_DataFromFIFO),.mlb(1'b1),.ss(spi_ce[1]),.sck(spi_sck),.sdin(spi_mosi), .sdout(spi_miso),.done(spi_rx2_done),.rdata(spi_rx2_recv));
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                         Decimation Rate Control common
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Decimation rates
-
-reg [1:0] speed;	// selected decimation rate in external program,
-
 localparam RATE48 = 6'd24;
 localparam RATE96  =  RATE48  >> 1;
 localparam RATE192 =  RATE96  >> 1;
 localparam RATE384 =  RATE192 >> 1;
-reg [5:0] rate;
 
-always @ (speed)
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                         Decimation Rate Control rx1
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Decimation rates
+
+reg [1:0] rx1_speed;	// selected decimation rate in external program,
+reg [5:0] rx1_rate;
+
+always @ (rx1_speed)
  begin 
-	  case (speed)
-	  0: rate <= RATE48;     
-	  1: rate <= RATE96;     
-	  2: rate <= RATE192;     
-	  3: rate <= RATE384;           
-	  default: rate <= RATE48;        
+	  case (rx1_speed)
+	  0: rx1_rate <= RATE48;     
+	  1: rx1_rate <= RATE96;     
+	  2: rx1_rate <= RATE192;     
+	  3: rx1_rate <= RATE384;           
+	  default: rx1_rate <= RATE48;        
 	  endcase
  end 
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                         Decimation Rate Control rx2
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Decimation rates
 
+reg [1:0] rx2_speed;	// selected decimation rate in external program,
+reg [5:0] rx2_rate;
+
+always @ (rx2_speed)
+ begin 
+	  case (rx2_speed)
+	  0: rx2_rate <= RATE48;     
+	  1: rx2_rate <= RATE96;     
+	  2: rx2_rate <= RATE192;     
+	  3: rx2_rate <= RATE384;           
+	  default: rx2_rate <= RATE48;        
+	  endcase
+ end 
+ 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 //                         GAIN Control
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -276,12 +318,30 @@ filter filter_inst(.clock(clk_10mhz), .frequency(sync_phase_word), .selected_fil
 wire   [31:0] sync_phase_word;
 wire  [63:0] ratio;
 
-reg[31:0] rxfreq;
+reg[31:0] rx1_freq;
 
 localparam M2 = 32'd1954687338; 	// B57 = 2^57.   M2 = B57/CLK_FREQ = 73728000
 localparam M3 = 32'd16777216;   	// M3 = 2^24, used to round the result
-assign ratio = rxfreq * M2 + M3; 
+assign ratio = rx1_freq * M2 + M3; 
 assign sync_phase_word = ratio[56:25]; 
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                         Convert frequency to phase word rx2
+//
+//		Calculates  ratio = fo/fs = frequency/73.728Mhz where frequency is in MHz
+//
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+wire   [31:0] sync_phase_word_rx2;
+wire  [63:0] ratio_rx2;
+
+reg[31:0] rx2_freq;
+
+localparam M4 = 32'd1954687338; 	// B57 = 2^57.   M2 = B57/CLK_FREQ = 73728000
+localparam M5 = 32'd16777216;   	// M3 = 2^24, used to round the result
+
+assign ratio_rx2 = rx2_freq * M4 + M5; 
+assign sync_phase_word_rx2 = ratio_rx2[56:25]; 
+
 
 //------------------------------------------------------------------------------
 //                           Software Reset Handler
@@ -289,8 +349,17 @@ assign sync_phase_word = ratio[56:25];
 wire reset;
 reset_handler reset_handler_inst(.clock(clk_10mhz), .reset(reset));
 
+//------------------------------------------------------------------------------
+//                           Pipeline for adc fanout
+//------------------------------------------------------------------------------
+reg [11:0] adcpipe [0:1];
+always @ (posedge adc_clock) begin
+    adcpipe[0] <= adc;
+    adcpipe[1] <= adc;
+end
+
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
-//                        Receiver module
+//                        Receiver module rx1 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 wire	[23:0] rx_I;
 wire	[23:0] rx_Q;
@@ -300,23 +369,50 @@ localparam CICRATE = 6'd08;
 
 receiver #(.CICRATE(CICRATE)) 
 		receiver_inst(	.clock(adc_clock),
-						.rate(rate), 
+						.rate(rx1_rate), 
 						.frequency(sync_phase_word),
 						.out_strobe(rx_strobe),
-						.in_data(adc),
+						.in_data(adcpipe[0]),
 						.out_data_I(rx_I),
 						.out_data_Q(rx_Q));
+						
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                        Receiver module rx2
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+wire	[23:0] rx2_I;
+wire	[23:0] rx2_Q;
+wire	rx2_strobe;
+
+localparam CICRATE_RX2 = 6'd08;
+
+receiver #(.CICRATE(CICRATE_RX2)) 
+		receiver_rx2_inst(	
+						.clock(adc_clock),
+						.rate(rx2_rate), 
+						.frequency(sync_phase_word_rx2),
+						.out_strobe(rx2_strobe),
+						.in_data(adcpipe[1]),
+						.out_data_I(rx2_I),
+						.out_data_Q(rx2_Q));			
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
-//                          rxFIFO Handler (IQ Samples)
+//                          rxFIFO Handler (IQ Samples) rx1
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 reg [47:0] rxDataFromFIFO;
 
 rxFIFO rxFIFO_inst(	.aclr(reset),
-							.wrclk(adc_clock),.data({rx_I, rx_Q}),.wrreq(rx_strobe), .wrempty(rxFIFOEmpty), 
-							.rdclk(spi_sck),.q(rxDataFromFIFO),.rdreq(spi_done));
+							.wrclk(adc_clock),.data({rx_I, rx_Q}),.wrreq(rx_strobe), .wrempty(rx1_FIFOEmpty), 
+							.rdclk(~spi_ce[0]),.q(rxDataFromFIFO),.rdreq(1'b1));
 
-						
+	
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                          rxFIFO Handler (IQ Samples) rx2
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+reg [47:0] rx2_DataFromFIFO;
+
+rxFIFO rx2_FIFO_inst(.aclr(reset),
+							.wrclk(adc_clock),.data({rx2_I, rx2_Q}),.wrreq(rx2_strobe), .wrempty(rx2_FIFOEmpty), 
+							.rdclk(~spi_ce[1]),.q(rx2_DataFromFIFO),.rdreq(1'b1));	 		
 				
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 //                          txFIFO Handler ( IQ-Transmit)
